e09df61aa0e0c3e42d188e89df94bc14e56462fa
jszprada <115928409+jszprada@users.noreply.github.com>
2023-04-28 18:47:36 +0200

    Upload files
    Upload files

---
+++ zd5.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+using namespace std;
+
+
+void zadanie5() {
+    int rozmiar;
+    cout << "Ktora liczbe doskonala chcesz znalezc: ";
+    cin >> rozmiar;
+    long int* liczby = new long int[rozmiar];
+
+    int znalezione = 0, x = 2, suma_dzielnikow = 0;
+
+    while (znalezione < rozmiar) {
+        for (int i = 1; i <= (x / 2); i++) {
+            if ((x % i) == 0)
+                suma_dzielnikow += i;
+        }
+
+        if (suma_dzielnikow == x) {
+            liczby[znalezione] = x;
+            ++znalezione;
+        }
+        ++++x;
+        suma_dzielnikow = 0;
+    }
+
+    cout << liczby[rozmiar - 1] << endl;    
+}
+int main()
+{
+    zadanie5();   
+}
+
+
